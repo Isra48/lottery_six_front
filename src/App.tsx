96f3ec5062f60e1d1aa45b5431ce04d6e32796f7
user.mail <israelalbarran48@gmail.com>
2024-12-05 02:57:08 -0600

    config(web-app): col2 added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Tittle from './assets/tittle.png'
 import Esfera from './assets/esfera.png'
+import Logos from './assets/logos.png'
 
 function App() {
 
@@ -11,12 +12,11 @@ function App() {
 
       <div className='container_father'>
 
+
         <div className='col1'>
           <div className='title_container'>
             <img src={Tittle} alt="imagen titulo" />
           </div>
-
-
           <div className="dropdown">
             <button className="dropbtn">Categoria</button>
             <div className="dropdown-content">
@@ -28,31 +28,45 @@ function App() {
               <a href="#">Movilidad VIP</a>
             </div>
           </div>
-
-           <div className='btn_sortear_container'>
-           <button className="button-42" role="button">Sortear</button>
-
-
-           </div>
-    
+          <div className='btn_sortear_container'>
+            <button className="button-42" role="button">Sortear</button>
+          </div>
           <div className='esfera_container'>
             <img src={Esfera} alt="imagen esfera" />
           </div>
-
-
-
         </div>
+
+
 
         <div className='col2'>
+          <div className='logos_container'>
+            <img src={Logos} alt="" />
+          </div>
 
+          <div className='tittle_premio'>
+             <p>Cargando...</p>
+          </div>
+
+          <div className='ganadores_container'>
+            <p>Isra</p>
+            <p>Gera</p>
+            <p>Isra</p>
+            <p>Gera</p>
+            <p>Isra</p>
+            <p>Gera</p>
+            <p>Isra</p>
+            <p>Gera</p>
+            <p>Isra</p>
+            <p>Gera</p>
+            <p>Isra</p>
+            <p>Gera</p>
+            
+            
+
+          </div>
 
 
         </div>
-
-
-
-
-
       </div>
 
       <div className='footer'> </div>
