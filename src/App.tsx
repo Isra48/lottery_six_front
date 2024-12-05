4078673f82f89268f92cb15e1925b0bf6ebd8e57
user.mail <israelalbarran48@gmail.com>
2024-12-05 03:39:12 -0600

    feat(web-app): added 2 select

--- src/App.tsx
+++ src/App.tsx
@@ -37,17 +37,26 @@ function App() {
             <div className='title_container'>
               <img src={Tittle} alt="imagen titulo" />
             </div>
-            <div className="dropdown">
-              <button className="dropbtn">Categoria</button>
-              <div className="dropdown-content">
-                <a href="#">Hogar</a>
-                <a href="#">Hogar VIP</a>
-                <a href="#">Conectividad</a>
-                <a href="#">Conectividad VIP</a>
-                <a href="#">Movilidad</a>
-                <a href="#">Movilidad VIP</a>
-              </div>
-            </div>
+         
+           <div className='select_container'>
+            <select>
+              <option selected value="0">Categoria:</option>
+              <option value="1">No Wrapper</option>
+              <option value="2">No JS</option>
+              <option value="3">Nice!</option>
+            </select>
+          </div>
+
+          <div className='select_container2'>
+            <select>
+              <option selected value="0">Premio:</option>
+              <option value="1">No Wrapper</option>
+              <option value="2">No JS</option>
+              <option value="3">Nice!</option>
+            </select>
+          </div>
+
+
             <div className='btn_sortear_container'>
               <button className="button-42" role="button">Sortear</button>
             </div>
