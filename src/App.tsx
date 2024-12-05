c48779249e518a52178648cb03a38d377ddb106a
user.mail <israelalbarran48@gmail.com>
2024-12-05 00:52:16 -0600

    config(web-app): setup

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+    <div className='container_father'>
+      <div className='col1'>
+
+      </div>
+
+      <div className='col2'>
+        hola
+
+      </div>
+
+
+
+
+
+    </div>
+
+    <div className='footer'> </div>
+    
+    </>
+  )
+}
+
+export default App
